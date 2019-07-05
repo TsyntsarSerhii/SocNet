a62289b867b2d30d6a9e1687fa5f4367a0a67727
SerhiyT <s.serhiy.t@gmail.com>
2019-07-05 14:46:20 +0300

    Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,40 @@
 import React from 'react';
 import styles from "./Users.module.css"
-const URL = 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQ3NjM5MTEzMTc5MjEwODI2/eminem_photo_by_dave_j_hogan_getty_images_entertainment_getty_187596325.jpg'
+import * as axios from 'axios';
+import userPHOTO from '../../assets/imeges/USER.png'
 
 let Users = (props) => {
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={u.id}>
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        });
+    }
+    return <div>
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.smaill: userPHOTO} alt="AVATAR" className={styles.userPhoto} />
+                    </div>
+                    <div>
+                        {u.followed
+                            ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
+                            : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                    </div>
+                </span>
+                <span>
                     <span>
-                        <div>
-                            <img src={u.photoUrl} alt="AVATAR" className={styles.userPhoto} />
-                        </div>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-                        </div>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
                     </span>
                     <span>
-                        <span>
-                            <div>{u.fullName}</div>
-                            <div>{u.status}</div>
-                        </span>
-                        <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
-                        </span>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
-                </div>)
-            }
-        </div>
-    )
+                </span>
+            </div>)
+        }
+    </div>
 }
 
 export default Users;
--- src/state/users-reduser.js
+++ src/state/users-reduser.js
@@ -1,14 +1,9 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
-const URLPHOTO = 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQ3NjM5MTEzMTc5MjEwODI2/eminem_photo_by_dave_j_hogan_getty_images_entertainment_getty_187596325.jpg'
 
 let initialState = {
-    users:[ 
-        { id: 1, photoUrl: URLPHOTO, followed: false, fullName: 'Serhii T', status: '123', location: { city: 'Hus', country: 'Ukr' } },
-        { id: 2, photoUrl: URLPHOTO, followed: false, fullName: 'And V', status: '123', location: { city: 'Lviv', country: 'Ukr' } },
-        { id: 3, photoUrl: URLPHOTO, followed: true, fullName: 'Pav V', status: '123', location: { city: 'Hus', country: 'Ukr' } }
-    ]
+    users: []
 };
 
 const usersReducer = (state = initialState, action) => {
