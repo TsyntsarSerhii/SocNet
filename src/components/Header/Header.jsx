3ee6b00fccbe88fdd91ae447209baa7e303aafce
SerhiyT <s.serhiy.t@gmail.com>
2019-07-14 16:34:00 +0300

    Revert "Authentication"
    This reverts commit eed6fe2f56d24f8daf864d66ffa9c92a7ab2e68f.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import s from './Header.module.css';
-import { NavLink } from 'react-router-dom';
 
-const Header = (props) => {
+const Header = () => {
     return <header className={s.header}>
-        <div className={s.loginBlock}>
-            {props.isAuth ? props.login
-            : <NavLink to={'/login'}>Login</NavLink>}
-        </div>
+        
     </header>
 }
 
